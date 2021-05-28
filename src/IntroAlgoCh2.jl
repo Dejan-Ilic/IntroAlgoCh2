bfe5dd6a803fd322677119e76c61a4add336495f
Dejan Ilic <dejan.ilic@vub.be>
2021-05-28 11:01:28 +0200

    Solved exercise with sumfinder

--- src/IntroAlgoCh2.jl
+++ src/IntroAlgoCh2.jl
@@ -59,7 +59,45 @@ function mergesort!(A::Array)
     return A
 end
 
-export insertionsort!, mergesort!
+function _binarysearch(S_sorted::Vector{Int}, target::Int, low::Int, high::Int)::Union{Nothing, Int}
+    if low == high
+        return (S_sorted[low] == target ? low : nothing)
+    end
+
+    mid = (low + high)÷2
+
+    if S_sorted[mid] == target
+        return mid
+    elseif S_sorted[mid] > target
+        return _binarysearch(S_sorted, target, low, mid)
+    else
+        return _binarysearch(S_sorted, target, mid, high)
+    end
+end
+
+function sumfinder(S::Vector{Int}, x::Int)::Union{Nothing, Tuple{Int, Int}}
+    mergesort!(S)
+
+    n = length(S)
+
+    for i=1:n-1
+        idx = let
+            target = x - S[i]
+            low = i+1
+            high = n
+
+            _binarysearch(S, target, low, high)
+        end
+        if idx !== nothing
+            return (i, idx)
+        end
+    end
+
+    return nothing
+
+end
+
+export insertionsort!, mergesort!, _binarysearch, sumfinder
 
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,9 @@
 using IntroAlgoCh2
 using Test
-using Random
+using Random, Plots
 
 @testset "IntroAlgoCh2.jl" begin
     r = 1:1000 |> collect |> shuffle!
     @test issorted(insertionsort!(r)) == true
     @test issorted(mergesort!(r)) == true
-
 end
